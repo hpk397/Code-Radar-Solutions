03b515c84042e38b74beb51c64bb593dea78bd27
Harmanpreet Kaur <135239684+hpk397@users.noreply.github.com>
2025-03-16 00:32:34 +0530

    Solution to Reverse Words in a String

---
+++ Reverse Words in a String@@676c039d80e1714600f6764c/code.c
@@ -0,0 +1,12 @@
+// Your code here...
+
+#include<stdio.h>
+int main(){
+    char a[100];
+    for(int i=0;i<100;i++){
+        scanf("%s",a);
+    }
+for(int i=n-1;i>=0;i--){
+    printf("%c",a[i]);
+}
+}
