0ef129ab5085699580d8a041b8526f6cd1ea1796
Harmanpreet Kaur <135239684+hpk397@users.noreply.github.com>
2025-03-31 20:07:24 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,18 +1,16 @@
 // Your code here...
 // Online C compiler to run C program online
 #include <stdio.h>
-int  fibonacciSeries(int n){
+void  fibonacciSeries(int n){
     int a=0;
-    if(n==1){
-        fibonacciSeries(n)=0;
-        return fibonacciSeries(n);
-    }
-    else if(n==2 || n==3){
-        fibonacciSeries(n)=1;
-        return fibonacciSeries(n);
-    }
-    else{
-        return  fibonacciSeries(n-1)+ fibonacciSeries(n-2);
-    }
-    }
+    int b=1;
+    int c;
+    int z=n;
+    while(n!=0){
+    c=a+b;
+    printf("%d ",c);
+    a=b;
+    b=c;
+
+        }    }
 
